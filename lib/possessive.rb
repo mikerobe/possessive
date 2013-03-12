2e93721ed33ce40bbed88bfc74f8a6b2d7d37d45
Mike Robertson <mikerobe@me.com>
2013-03-11 20:52:10 -0400

    add inverse and special cases

--- lib/possessive.rb
+++ lib/possessive.rb
@@ -3,10 +3,26 @@
 String.class_eval do
   def possessive
     return self if self.empty?
-    self + ('s' == self[-1,1] ? Possessive::APOSTROPHE_CHAR : Possessive::APOSTROPHE_CHAR+"s")
+    case self
+    when "I" then "my"
+    when /^([yY])ou$/ then "#{$1}our"
+    when /s$/ then self + Possessive::APOSTROPHE
+    else self + Possessive::APOSTROPHE + "s"
+    end
+  end
+
+  def from_possessive
+    return self if self.empty?
+    case self
+    when /^my$/i then "I"
+    when /^([yY])our$/i then "#{$1}ou"
+    when /(.*)#{Possessive::APOSTROPHE}/ then $1
+    when /(.*)'/ then $1
+    else self
+    end
   end
 end
 
 module Possessive
-  APOSTROPHE_CHAR = '’'
+  APOSTROPHE = '’'
 end
